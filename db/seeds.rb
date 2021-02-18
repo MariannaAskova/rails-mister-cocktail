5f3b9254625674105ddf5bd81f3c028025d7a614
Marianna Askova <marianna.askova@gmail.com>
2021-02-18 16:58:36 -0600

    Half way through

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Ingredient.destroy_all
+
+puts "Creating ingredients..."
+lemon = { name: "lemon" }
+ice =  { name: "ice" }
+mint_leaves = { name: "mint_leaves" }
+tequila =  { name: "tequila" }
+
+[ lemon, ice, mint_leaves, tequila].each do |attributes|
+  ingredient = Ingredient.create!(attributes)
+  puts "Created #{ingredient.name}"
+end
+puts "Finished!"
